ac71ae6fe574b0f2d8de9c6c2968b01c6a0163cd
Domantas <domantas.mauruca@gmail.com>
2023-01-15 12:43:48 +0100

    Add Cypress and automated testing

---
+++ packages/ui/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	video: false,
+	screenshotOnRunFailure: false,
+	component: {
+		devServer: {
+			framework: "react",
+			bundler: "vite",
+		},
+	},
+});
